17c5f3855e90d4a22fa23fa886a646a801306fee
Jameson.Brown <jameson.brown@mx.com>
2021-06-03 20:44:57 -0600

    Added backend/server

---
+++ src/server/lib/getExactMatches.js
@@ -0,0 +1,48 @@
+const incrementPegValueTotal = require('./incrementPegValueTotal');
+const { _MATCHES_EXACT_, _MATCHES_NONE_ } = require('./constants');
+
+/**
+ * Find how many exact matches of both peg value and placement the player has
+ * guessed.
+ * @param  {Array}  codeGuess     An array containing the player's guess.
+ * @param  {Number} numberOfPegs  The number of pegs that make up the code.
+ * @param  {Array}  secretCode    The secret code the player is trying to crack.
+ * @return {Object}               An object containing the matches, total of each
+ *                                peg value, and total exact matches found in this
+ *                                turn.
+ */
+function getExactMatches (codeGuess, numberOfPegs, secretCode) {
+	const guessMatches = [];
+	const guessedPegValueTotals = [];
+	let totalExactMatches = 0;
+
+	/**
+	 * Initialise the guessed peg value totals so that there's a property for each
+	 * peg value in the code.
+	 * */
+	secretCode.forEach((pegValue) => {
+		guessedPegValueTotals[pegValue] = 0;
+	});
+
+	/**
+	 * Loop through the secret code and the player's guess to find out how many
+	 * of the pegs match perfectly.
+	 */
+	for (let i=0; i<numberOfPegs; i++){
+		if (codeGuess[i] === secretCode[i]){
+			guessMatches[i] = _MATCHES_EXACT_;
+			totalExactMatches++;
+			guessedPegValueTotals[secretCode[i]] = incrementPegValueTotal(guessedPegValueTotals[secretCode[i]]);
+		} else {
+			guessMatches[i] = _MATCHES_NONE_;
+		}
+	}
+
+	return {
+		guessMatches: guessMatches,
+		pegValueTotals: guessedPegValueTotals,
+		totalExactMatches: totalExactMatches
+	};
+}
+
+module.exports = getExactMatches;
---
+++ src/server/lib/getPartialMatches.js
@@ -0,0 +1,49 @@
+// ToDo: See if we still need pegValueTotals
+
+const incrementPegValueTotal = require('./incrementPegValueTotal');
+const { _MATCHES_NONE_, _MATCHES_PARTIAL_, _MATCHES_EXACT_ } =  require('./constants');
+
+/**
+ * Find how many matches of peg value, but not placement the player has guessed.
+ * @param  {Object} exactMatchResults Contains the exact matches and the total of
+ *                                    each peg value found for this turn.
+ * @param  {Array}  codeGuess         An array containing the player's guess.
+ * @param  {Number} numberOfPegs      The number of pegs that make up the code.
+ * @param  {Array}  secretCode        The secret code the player is trying to crack.
+ * @param  {Array}  pegValueTotals    An array containing the total of each peg
+ *                                    value in the secret code.
+ * @return {Object} An object containing the matches, total of each peg value, total
+ *                  exact matches, and total partial matches found in this turn.
+ */
+function getPartialMatches (exactMatchResults, codeGuess, numberOfPegs, secretCode, pegValueTotals) {
+	const guessMatches = exactMatchResults.guessMatches;
+	const alreadyMatched = exactMatchResults.guessMatches.map(match => match === _MATCHES_EXACT_);
+	const guessedPegValueTotals = exactMatchResults.pegValueTotals;
+	let totalPartialMatches = 0;
+
+	// Loop through the guesses
+	for (let i=0; i<numberOfPegs; i++){
+		// Only check the guess if it was unsucessful in the previous loop.
+		if (guessMatches[i] === _MATCHES_NONE_){
+			// Loop through the secret code
+			for (let c=0; c<numberOfPegs; c++){
+				if (!alreadyMatched[c] && codeGuess[i] === secretCode[c] && guessedPegValueTotals[secretCode[c]] < pegValueTotals[secretCode[c]]){
+					guessMatches[i] = _MATCHES_PARTIAL_;
+					alreadyMatched[c] = true;
+					totalPartialMatches++;
+					guessedPegValueTotals[secretCode[c]] = incrementPegValueTotal(guessedPegValueTotals[secretCode[c]]);
+					break;
+				}
+			}
+		}
+	}
+
+	return {
+		guessMatches: guessMatches,
+		pegValueTotals: guessedPegValueTotals,
+		totalExactMatches: exactMatchResults.totalExactMatches,
+		totalPartialMatches: totalPartialMatches
+	};
+}
+
+module.exports = getPartialMatches;
